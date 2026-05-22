5e0c98f3d9270dde40d5ae55c07c27bc8dd36b6e
chrollo-lucifer-12 <sahilpython@gmail.com>
2026-05-22 07:41:23 +0000

    add allkeys random

--- config/config.go
+++ config/config.go
@@ -4,3 +4,7 @@ var Host string
 var Port int
 var KeyLimit int
 var AOFFILE string = "./appendonly.aof"
+
+var KeysLimit int = 100
+var EvictionRatio float64 = 0.40
+var EvictionStrategy string = "allkeys-random"
--- core/eviction.go
+++ core/eviction.go
@@ -1,5 +1,7 @@
 package core
 
+import "github.com/redis-server/config"
+
 func evictFirst() {
 	for k := range store {
 		delete(store, k)
@@ -7,6 +9,23 @@ func evictFirst() {
 	}
 }
 
+func evictAllKeysRandom() {
+	evictCount := int64(config.EvictionRatio * float64(config.KeyLimit))
+
+	for k := range store {
+		Del(k)
+		evictCount--
+		if evictCount <= 0 {
+			break
+		}
+	}
+}
+
 func evict() {
-	evictFirst()
+	switch config.EvictionStrategy {
+	case "allkeys-random":
+		evictAllKeysRandom()
+	default:
+		evictFirst()
+	}
 }
