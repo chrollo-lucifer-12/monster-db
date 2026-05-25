550bc768699748556dc0e02df2ba4cd2138d3a90
chrollo-lucifer-12 <sahilpython@gmail.com>
2026-05-25 15:12:41 +0000

    add async server runner

--- server/async_server_2.go
+++ server/async_server_2.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"log"
+	"net"
+	"runtime"
 
+	"github.com/redis-server/config"
 	"golang.org/x/sys/unix"
 )
 
@@ -44,4 +47,48 @@ func AcceptTcpHandler(el *EventLoop, serverFD int, clientData interface{}) {
 
 func ReadQueryFromClient(loop *EventLoop, fd int, clientData interface{}) {
 	log.Printf("Data available to read on client FD: %d\n", fd)
+}
+
+func RunAsyncServer2() error {
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	log.Println("Starting server on ", config.Host, config.Port)
+
+	maxClients := 100000
+	loop, err := CreateEventLoop(maxClients)
+	if err != nil {
+		return err
+	}
+	defer unix.Close(loop.EpollFD)
+
+	serverFD, err := unix.Socket(unix.AF_INET, unix.O_NONBLOCK|unix.SOCK_STREAM, 0)
+	if err != nil {
+		return err
+	}
+	defer unix.Close(serverFD)
+
+	unix.SetsockoptInt(serverFD, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
+
+	ip4 := net.ParseIP(config.Host)
+	err = unix.Bind(serverFD, &unix.SockaddrInet4{
+		Port: config.Port,
+		Addr: [4]byte{ip4[0], ip4[1], ip4[2], ip4[3]},
+	})
+	if err != nil {
+		return err
+	}
+
+	if err = unix.Listen(serverFD, maxClients); err != nil {
+		return err
+	}
+
+	err = loop.AddFileEvent(serverFD, unix.EPOLLIN, AcceptTcpHandler, nil)
+	if err != nil {
+		return err
+	}
+
+	loop.Main()
+
+	return nil
 }
