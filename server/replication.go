f0d1603304a2823cd872885bc7e32be272b989e8
chrollo-lucifer-12 <sahilpython@gmail.com>
2026-06-12 13:33:04 +0000

    handle psync

--- server/replication.go
+++ server/replication.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"strconv"
@@ -127,5 +128,50 @@ func HandleMasterReadableHandshake(loop *EventLoop, fd int, clientData any) {
 			ReplicaState = REPL_STATE_RECEIVING_REPLCONF
 			clientsPendingWrite[fd] = client
 		}
+
+	case REPL_STATE_RECEIVING_REPLCONF:
+		if strings.Contains(string(client.QueryBuf), "+OK") {
+			client.QueryBuf = client.QueryBuf[:0]
+
+			psynCmd := fmt.Sprintf("PSYNC %s %d\r\n", MasterReplID, CachedOffset)
+			client.ReplyBuf = append(client.ReplyBuf, []byte(psynCmd)...)
+
+			ReplicaState = REPL_STATE_RECEIVING_PSYNC
+			clientsPendingWrite[fd] = client
+		}
+
+	case REPL_STATE_RECEIVING_PSYNC:
+		response := string(client.QueryBuf)
+		if strings.HasPrefix(response, "+FULLRESYNC") {
+			parts := strings.Split(strings.TrimSpace(response), " ")
+			MasterReplID = parts[1]
+			CachedOffset, _ = strconv.ParseInt(parts[2], 10, 64)
+
+			log.Printf("Handshake validated! Synchronizing Master ID: %s starting at Offset: %d\n", MasterReplID, CachedOffset)
+			client.QueryBuf = client.QueryBuf[:0]
+
+			ReplicaState = REPL_STATE_ONLINE
+			loop.AddFileEvent(fd, unix.EPOLLIN, ReadLiveReplicationStream, client)
+		}
+
 	}
+}
+
+func ReadLiveReplicationStream(loop *EventLoop, fd int, clientData any) {
+	client := clientData.(*Client)
+
+	var buf [4096]byte
+	n, err := unix.Read(fd, buf[:])
+	if err != nil || n <= 0 {
+		log.Println("Connection to master broken. Re-entering connection mode.")
+		cleanupClient(client, loop)
+		ReplicaState = REPL_STATE_CONNECT
+		return
+	}
+
+	client.QueryBuf = append(client.QueryBuf, buf[:n]...)
+	CachedOffset += int64(n)
+
+	fmt.Printf("[Replica Log Ingest] Parsed %d bytes from Master stream\n", n)
+	client.QueryBuf = client.QueryBuf[:0]
 }
